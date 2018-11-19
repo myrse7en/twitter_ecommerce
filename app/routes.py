8111138da556c11177753e29f232fced8d417eac
Mayur Bhatia <23645213+myrse7en@users.noreply.github.com>
2018-11-19 14:11:02 -0500

    Home page completed, merge with master

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,43 @@ from flask import render_template, url_for, redirect
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    products = {
+        101: {
+            "id": 101,
+            "title": "Soap",
+            "price": 4.95,
+            "url": "http://placehold.it/250x250",
+            "desc": "This bar of soap is a bar of soapy soap."
+        },
+        102: {
+            "id": 102,
+            "title": "Grapes",
+            "price": 3.85,
+            "url": "http://placehold.it/250x250",
+            "desc": "These grapes are abundle of grapey grapes."
+        },
+        103: {
+            "id": 103,
+            "title": "Oranges",
+            "price": 67.85,
+            "url": "http://placehold.it/250x250",
+            "desc": "This box is a box of orangey oranges."
+        },
+        104: {
+            "id": 104,
+            "title": "Oranges",
+            "price": 67.85,
+            "url": "http://placehold.it/250x250",
+            "desc": "This box is a box of orangey oranges."
+        },
+        105: {
+            "id": 105,
+            "title": "Oranges",
+            "price": 67.85,
+            "url": "http://placehold.it/250x250",
+            "desc": "This box is a box of orangey oranges."
+        }
+    }
+
+
+    return render_template('index.html', products=products)
