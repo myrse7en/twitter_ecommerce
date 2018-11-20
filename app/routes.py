6b5cd7e3f8973872d6ca720b8c6877a7c8346215
Mayur Bhatia <23645213+myrse7en@users.noreply.github.com>
2018-11-20 10:22:24 -0500

    Posts page showing bio and tweets for proper profile, accessed through url parameter, add form capabilities next

--- app/routes.py
+++ app/routes.py
@@ -48,3 +48,64 @@ def index():
 
 
     return render_template('index.html', products=products)
+
+@app.route('/posts')
+@app.route('/posts/<name>')
+def posts(name='Max'):
+    posts_dict = {
+        0: {
+            'date': 'Sept. 9th, 2018',
+            'name': 'Max',
+            'tweet': 'Today I had cereal for breakfast.'
+        },
+        1: {
+            'date': 'July 1st, 2018',
+            'name': 'Kelly',
+            'tweet': 'Went for a run downtown.'
+        },
+        2: {
+            'date': 'June 21st, 2018',
+            'name': 'Max',
+            'tweet': 'Got a new job!! Working for the man.'
+        },
+        3: {
+            'date': 'March 4th, 2018',
+            'name': 'Kelly',
+            'tweet': 'Hiking is fun, get outside.'
+        },
+        4: {
+            'date': 'February 8th, 2018',
+            'name': 'Kelly',
+            'tweet': 'This is a sample text. This is a sample text.'
+        },
+        5: {
+            'date': 'October 10th, 2017',
+            'name': 'Max',
+            'tweet': 'This is a sample text. This is a sample text.'
+        },
+        6: {
+            'date': 'October 1st, 2017',
+            'name': 'Max',
+            'tweet': 'This is a sample text. This is a sample text.'
+        },
+        7: {
+            'date': 'Sept. 31st, 2017',
+            'name': 'Kelly',
+            'tweet': 'This is a sample text. This is a sample text.'
+        }
+    }
+    people = {
+        0: {
+            'name': 'Max',
+            'age': 26,
+            'bio': 'Avid swimmer, cat lover, and I ride a bike everywhere.',
+            'url': 'http://placehold.it/250x250'
+        },
+        1: {
+            'name': 'Kelly',
+            'age': 21,
+            'bio': 'My name is Kelly, I work in Boston and love dogs <3.',
+            'url': 'http://placehold.it/250x250'
+        }
+    }
+    return render_template('posts.html', posts=posts_dict, people=people, name=name)
